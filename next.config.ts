7ff76bb2fe935642237fb1531ae549129bab0557
Nico Swan <hi@nicoswan.com>
2025-02-15 19:55:14 +0200

    asdas

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /**
-   * Enable static exports.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-  output: 'export',
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
+  distDir: 'build',
 };
 
 export default nextConfig;
