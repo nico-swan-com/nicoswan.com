e1bcf690cb16410b601fe7a72996a2190b306d78
Nico Swan <hi@nicoswan.com>
2025-02-17 00:00:29 +0200

    build: added standalone build

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,29 @@
 import type { NextConfig } from "next";
 
-const { BUILD_TYPE } = process.env
+const { BUILD_TYPE } = process.env;
 
 const defaultConfig: NextConfig = {
-  distDir: '_next'
+  distDir: "_next",
 };
 
 const customServerConfig: NextConfig = {
-  distDir: 'standalone',
+  distDir: "_next",
   /**
-  * Enable static exports.
-  *
-  * @see https://nextjs.org/docs/app/building-your-application/configuring/custom-server
-  */
-  output: 'standalone',
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/configuring/custom-server
+   */
+  output: "standalone",
 };
 
 const staticConfig: NextConfig = {
   /**
-  * Enable static exports.
-  *
-  * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-  */
-  output: 'export',
-  
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
@@ -35,16 +35,16 @@ const staticConfig: NextConfig = {
   },
 };
 
-let nextConfig = defaultConfig
+let nextConfig = defaultConfig;
 switch (String(BUILD_TYPE).toUpperCase()) {
-  case 'STATIC':
-    nextConfig = staticConfig
+  case "STATIC":
+    nextConfig = staticConfig;
     break;
-    case 'CPANEL CUSTOM SERVER':
-      nextConfig = customServerConfig
-      break;
+  case "CPANEL CUSTOM SERVER":
+    nextConfig = customServerConfig;
+    break;
   default:
-    nextConfig = defaultConfig
+    nextConfig = defaultConfig;
     break;
 }
 
