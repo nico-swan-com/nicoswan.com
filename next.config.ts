44ae01269746c064cfe7581dfa9201b9c21b5786
Nico Swan <hi@nicoswan.com>
2025-02-16 13:49:13 +0200

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const { NODE_ENV } = process.env
+
+const defaultConfig: NextConfig = {
   distDir: '_next'
 };
+
+const staticConfig: NextConfig = {
+  /**
+  * Enable static exports.
+  *
+  * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+  */
+  output: 'export',
+  
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+};
+
+const nextConfig = String(NODE_ENV).toUpperCase() === 'STATIC' ? staticConfig : defaultConfig  
 
 export default nextConfig;
